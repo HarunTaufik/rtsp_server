264ccaa5a00b4b646e5e0c49558a7d91f106907c
HarunTaufik <111112134+HarunTaufik@users.noreply.github.com>
2024-08-27 23:25:23 +0700

    Update rtsp_cam.py

--- rtsp_cam.py
+++ rtsp_cam.py
@@ -1,27 +1,26 @@
-from gi.repository import Gst, GstRtspServer
-import sys
+#!/usr/bin/env python3
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject, GstRtspServer
 
 Gst.init(None)
 
 class RTSPServer(GstRtspServer.RTSPServer):
     def __init__(self):
         super().__init__()
-        self.set_service("8554")  # Port RTSP server
-        self.get_mount_points().add_factory("/camera", self.create_pipeline())
+        self.set_address('0.0.0.0')  # Bind to all interfaces
+        self.set_service('8554')     # Default RTSP port
 
-    def create_pipeline(self):
-        factory = GstRtspServer.RTSPMediaFactory()
-        factory.set_launch(
-            "v4l2src ! video/x-raw, width=640, height=480 ! videoconvert ! x264enc ! rtph264pay name=pay0"
-        )
-        factory.set_shared(True)
-        return factory
+        self.factory = GstRtspServer.RTSPMediaFactory()
+        self.factory.set_launch("( v4l2src ! videoconvert ! x264enc ! rtph264pay name=pay0 )")
+        self.factory.set_shared(True)
 
-def main():
+        self.get_mount_points().add_factory('/stream', self.factory)
+
+        self.attach(None)
+
+if __name__ == '__main__':
     server = RTSPServer()
-    server.attach(None)
     loop = GObject.MainLoop()
     loop.run()
-
-if __name__ == "__main__":
-    main()
